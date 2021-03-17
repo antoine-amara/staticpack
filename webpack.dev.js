243822e1d1541995f9f10dd020729bf2c31a2756
Antoine AMARA <amara.antoine@gmail.com>
2021-03-17 12:47:43 +0100

    feat: migrate to webpack 5
    upgrade all packages and especially webpack and the associated packages
    to the last major version.
    migrate all configurations to be compatible with webpack 5.
    Note: add the 'target: web' option in the dev config
    to be able to have the hot reload.
    it is due to a bug on webpack dev server and webpack 5.
    upgrade the nodejs image with the new LTS version.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,14 @@
-// webpack v4
+// webpack v5
 // specific rules to build development bundle.
 
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
-const { HotModuleReplacementPlugin } = require('webpack')
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [
-    new HotModuleReplacementPlugin({})
-  ],
+  target: 'web',
   devServer: {
     host: '0.0.0.0',
     port: 9000,
